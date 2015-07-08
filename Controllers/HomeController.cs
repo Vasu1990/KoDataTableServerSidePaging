3c58bc3453f1b4c93060e8c927add4e78bf68954
unknown <vasunagpal@vasu368301.Nagarro.local>
2015-07-08 22:18:15 +0530

    Initial commit vasu

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KODatatableServerSidePaging.Models;
+
+namespace KODatatableServerSidePaging.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetCollection(GlobalSearchText GlobalSearchText, int RecordsToTake, int RecordsToSkip, List<DataTableConfig> Columns)
+        {
+            List<TableModel> objLs = new List<TableModel>();
+            for (int i = 0; i < 12; i++)
+            {
+                TableModel obj = new TableModel();
+                obj.Id = i;
+                obj.Address = "Address" + i;
+                obj.Name = "vasu" + i;
+                objLs.Add(obj);
+            }
+            return Json(objLs);
+        }
+
+    }
+
+   
+}
---
+++ Models/DatatTablepagingModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KODatatableServerSidePaging.Models
+{
+    
+        public class TableModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+        }
+        public class GlobalSearchText
+        {
+            public string value { get; set; }
+            public string regex { get; set; }
+        }
+        public class DataTableConfig
+        {
+            public string ColumnName { get; set; }
+            public bool IsSorted { get; set; }
+            public string SearchText { get; set; }
+            public bool IsSearchable { get; set; }
+            public string SortDirection { get; set; }
+
+        }
+    
+}
